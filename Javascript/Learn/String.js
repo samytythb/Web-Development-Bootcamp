054c7c380eac2992b537e2d1354c957515e60a3f
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-10 01:27:59 +0700

    update

--- Javascript/Learn/String.js
+++ Javascript/Learn/String.js
@@ -3,5 +3,14 @@
 // myName = myName.toUpperCase();
 // myName = myName.toLowerCase();
 function bmiCalculator(weight, height) {
-  return (weight / height) * height;
+  var bmi = weight / (heigh * height);
+  var interpretation;
+  if (bmi > 18.5) {
+    interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+  } else {
+    interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+  }
+  return interpretation;
 }
