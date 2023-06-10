dd62fb60fa033e774e55fd0ae736afe17bc72b21
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-11 04:19:53 +0700

    update

--- Javascript/Project/Drum Kit/index.js
+++ Javascript/Project/Drum Kit/index.js
@@ -1,6 +1,13 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", handleClick);
-  function handleClick() {
-    alert("hello");
-  }
+var sound = ["tom-1", "tom-2", "tom-3", "tom-4", "crash", "kick-bass", "snare"];
+var buttons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", handleClick);
+  buttons[i].dataset.sound = "sounds/" + sound[i] + ".mp3";
+}
+
+function handleClick() {
+  this.style.color = "white";
+  var audio = new Audio(this.dataset.sound);
+  audio.play();
 }
