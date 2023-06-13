29b08a5bab56ca3db47229e238ecf60c1a0861ca
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-13 23:43:55 +0700

    update

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,9 @@
-$(document).ready(function () {
-  // check jQuery exist . for script jQuery in head of HTML file
-  $("h1").css("color", "red");
-});
+// $(document).ready(function () {
+//   // check jQuery exist . for script jQuery in head of HTML file
+//   $("h1").css("color", "red");
+// });
+$("h1").addClass("leduc mg-50");
+$("h1").removeClass("leduc mg-50");
+$("h1").hasClass("leduc mg-50");
+$("h1").text("Bye");
+$("h1").html("<em>Duc</em>");
