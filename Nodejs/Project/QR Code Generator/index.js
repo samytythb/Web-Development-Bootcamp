040580cc70f20f049826fd69733e61d6700b0012
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-18 16:09:25 +0700

    update

---
+++ Nodejs/Project/QR Code Generator/index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+inquirer
+  .prompt([{ message: "Type in your URL:", name: "URL" }])
+  .then((answers) => {
+    console.log(answers);
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_portfolio.png"));
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+    } else {
+      // Something else went wrong
+    }
+  });
