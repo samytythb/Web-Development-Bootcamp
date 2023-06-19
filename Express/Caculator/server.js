8f4dfe7917d47535b525953558aaff95803764fb
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-19 23:38:14 +0700

    update

--- Express/Caculator/server.js
+++ Express/Caculator/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import path from "path";
+import bodyParser from "body-parser";
 const app = express();
+
 const port = 3000;
 const __dirname = path.resolve();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 app.post("/", (req, res) => {
-  res.send("Thanks you");
+  let result = Number(req.body.num1) + Number(req.body.num2);
+  res.send(`<h1>Result : ${result}</h1>`);
 });
 app.listen(port, () => {
   console.log("Listening on port" + port);
