3230ad7d997332b85640a3826964c772faa77b4b
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-19 23:23:10 +0700

    update

--- Express/Caculator/server.js
+++ Express/Caculator/server.js
@@ -6,6 +6,9 @@ const __dirname = path.resolve();
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+app.post("/", (req, res) => {
+  res.send("Thanks you");
+});
 app.listen(port, () => {
   console.log("Listening on port" + port);
 });
