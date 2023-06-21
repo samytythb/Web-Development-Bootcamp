ad4b1bfcdfe8f0aebf66215f9ec6cf982968f471
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-21 09:39:03 +0700

    update

--- API/Projects/Newsletter-Signup/server.js
+++ API/Projects/Newsletter-Signup/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import request from "request";
+import https from "https";
 const app = express();
 import bodyParser from "body-parser";
 import path from "path";
@@ -11,6 +13,28 @@ app.post("/", (req, res) => {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var email = req.body.email;
+  var data = {
+    FNAME: firstName,
+    LNAME: lastName,
+  };
+  var jsonData = JSON.stringify(data);
+  const url = "";
+  const options = {
+    method: "POST",
+    auth: "",
+  };
+  const request = https.request(url, option, (response) => {
+    if (response.statusCode === 200) {
+      res.send("Success");
+    } else {
+      res.send("Pls try again");
+    }
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
 app.listen(3000, () => {
   console.log("listening on port 3000");
