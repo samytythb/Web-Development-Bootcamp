0965654d6a2f3c10f4ecc49de8186fc74bcc1371
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-25 23:40:39 +0700

    update

---
+++ Reactjs/Projects/Keeper App/keeper-app/src/note.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content: "0. How many programmers does it take to change a light bulb?",
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content: "How to keep programmer in the shower forever?",
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content: "Why did the programmer quit his job?",
+  },
+  {
+    key: 4,
+    title: "Big ideas",
+    content: "Learn mor about IT",
+  },
+];
