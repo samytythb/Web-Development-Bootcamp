aebb581d5122d4eea9fe7d375bc3245ccb212192
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-06-27 23:55:16 +0700

    update

--- Reactjs/Learn/incresae-number/src/components/App.jsx
+++ Reactjs/Learn/incresae-number/src/components/App.jsx
@@ -1,9 +1,15 @@
-var count =0;
-function increase(){
-    count++;
-}
+import React, { useState } from "react";
 function App (){
+    const [count,setCount]=useState(0);
+    console.log(count);
+function increase(){
+    setCount(count+1)
+}
+function decrease(){
+    setCount(count-1);
+}
     return <div><h1>{count}</h1>
-    <button onClick={increase}>+</button></div>
+    <button onClick={increase}>+</button>
+    <button onClick={decrease}>-</button></div>
 }
 export default App;
