2f20147768adafb4cb88c7c18d1a85085a58ad4e
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-07-02 00:27:32 +0700

    update

--- Reactjs/Learn/handle-event/src/components/App.jsx
+++ Reactjs/Learn/handle-event/src/components/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react"
 
 function App(){
-  
+    const [headingText, setHeadingText]=useState("")
     const [isMouseOver,setIsMouseOver]=useState(false)
     const [name ,setName]=useState("")
-    
+    function setMyName(event){
+        
+        setHeadingText(name)
+        event.preventDefault()
+    }
     function changeBtn(){
         isMouseOver?
         setIsMouseOver(false):setIsMouseOver(true)
@@ -13,9 +17,11 @@ function App(){
        setName(event.target.value);
     }
     return <div>
-        <h1>Hello{name}</h1>
+        <h1>Hello {headingText}</h1>
+        <form onSubmit={setMyName}>
         <input onChange={handleChange} type="text" placeholder="Enter your name" value={name} />
         <button style={{backgroundColor:isMouseOver ?"black":"white",color:  isMouseOver?"white":"black"}} type="submit" onMouseOver={changeBtn} onMouseOut={changeBtn}>Submit</button>
+        </form>
     </div>
 }
 export default App;
