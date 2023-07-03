773b16bd8e2624e90acd6c75a02bf13db6211280
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-07-03 23:23:31 +0700

    update

--- Reactjs/Projects/Keeper App/keeper-app/src/components/App.jsx
+++ Reactjs/Projects/Keeper App/keeper-app/src/components/App.jsx
@@ -1,17 +1,29 @@
-import react, { useState } from "react";
+import react, { useState, useEffect } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 import notes from "../note.js";
 import CreateArea from "./CreateArea.jsx";
+import Zoom from "@mui/material/Zoom";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    const storedNotes = localStorage.getItem("notes");
+    if (storedNotes) {
+      setNotes(JSON.parse(storedNotes));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
   function addNote(newNote) {
     setNotes((prevNotes) => {
       return [...prevNotes, newNote];
     });
   }
+
   function deleteNote(id) {
     setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
--- Reactjs/Projects/Keeper App/keeper-app/src/components/CreateArea.jsx
+++ Reactjs/Projects/Keeper App/keeper-app/src/components/CreateArea.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
+  const [isExp, setExp] = useState(false);
   function handleChange(event) {
     const { name, value } = event.target;
     setNote((prevNote) => {
@@ -16,29 +18,40 @@ function CreateArea(props) {
   }
   function submitNote(event) {
     props.onAdd(note);
-
     event.preventDefault();
+  }
+  function expand() {
+    setExp(true);
   }
   return (
     <div>
       <form>
-        <input
-          className="p-note"
-          name="title"
-          type="text"
-          onChange={handleChange}
-          placeholder="Title"
-          value={note.title}
-        />
+        {isExp ? (
+          <input
+            className="p-note"
+            name="title"
+            type="text"
+            onChange={handleChange}
+            placeholder="Title"
+            value={note.title}
+            autoFocus="true"
+            autoComplete="true"
+          />
+        ) : null}
         <textarea
           className="p-note"
           name="content"
-          rows="3"
+          rows={isExp ? 3 : 2}
+          onClick={expand}
           onChange={handleChange}
           placeholder="Take a note..."
           value={note.content}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={true}>
+          <button onClick={submitNote}>
+            <AddIcon />
+          </button>
+        </Zoom>
       </form>
     </div>
   );
